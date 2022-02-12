e857a2c9c404117a3af95499773c3e5a01ae3f24
McAuley Penney <jacobmpenney@gmail.com>
2022-02-12 14:46:00 -0700

    updates

--- colors/still_light.lua
+++ colors/still_light.lua
@@ -1,47 +1,44 @@
 --[[
-        Name  : Cacophony
-        Author: MP
-        Notes:
-            for highlight groups, see https://neovim.io/doc/user/syntax.html#highlight-groups
-            background color: #212121
+    Author: MP
+    Notes:
+        for highlight groups, see https://neovim.io/doc/user/syntax.html#highlight-groups
+        background color: #212121
+
+    TODO:
+        • hi Lua nil, Python None
+            • also highlights all pairs?
 ]]
 
 local na = {}
 local black_0 = "#000000"
-local black_1 = "#15181C"
+local black_1 = "#141414"
 local black_2 = "#212121"
 local black_3 = "#2d2d2d"
-local black_4 = "#666666"
-local black_5 = "#c1c1c1"
+local black_4 = "#424242"
+local black_5 = "#666666"
+local black_6 = "#c1c1c1"
 
 local white_0 = "#DBD3C9"
 local white_1 = "#FFFFFF"
 
-local green_0 = "#5C8145"
-local green_1 = "#B3C3A4"
+local green_1 = "#5C8145"
+local green_2 = "#B3C3A4"
 
 local blue_0 = "#434649"
 local blue_1 = "#444D5F"
-local blue_2 = "#4C566A"
-local blue_3 = "#616E88"
-local blue_4 = "#8296b0"
-local blue_5 = "#8D98A7"
+local blue_2 = "#616E88"
+local blue_3 = "#8296b0"
+local blue_4 = "#8D98A7"
 
 local red_0 = "#e26d5c"
 local red_2 = "#FF0000"
 
-local purple_0 = "#4F5F98"
-local purple_1 = "#90708C"
+local purple_0 = "#90708C"
 
 local yellow_0 = "#FEAD4C"
 local yellow_1 = "#FFC15E"
 
 local cyan_0 = "#00CECB"
-
--- styles
-local bold = "bold"
-local ital = "italic"
-local ul = "underline"
 
 -- syntax metaarray
 local hi = {}
@@ -50,34 +47,32 @@ local links = {}
 -- ui
 hi["ColorColumn"] = na
 hi["Conceal"] = na
-hi["CursorLine"] = { fg = black_0, bg = yellow_1, gui = bold } -- vim-illuminate
+hi["CursorLine"] = { fg = yellow_1, bg = black_4, bold = true } -- vim-illuminate
 hi["CursorLineNr"] = na
-hi["Directory"] = { fg = purple_1 }
+hi["Directory"] = { fg = blue_3 }
 hi["EndOfBuffer"] = { fg = white_1 }
 hi["ErrorMsg"] = { fg = red_2 }
 hi["FoldColumn"] = na
-hi["Folded"] = { fg = blue_5, gui = ital }
+hi["Folded"] = { fg = blue_0, italic = true }
 hi["LineNr"] = { fg = blue_0 }
-hi["MatchParen"] = { fg = red_2, gui = bold }
-hi["ModeMsg"] = { fg = green_0 } -- remove bolding on macro recording notification
-hi["MoreMsg"] = { fg = purple_1 }
+hi["MatchParen"] = { fg = red_2, bold = true }
+hi["ModeMsg"] = { fg = green_1 } -- remove bolding on macro recording notification
+hi["MoreMsg"] = { fg = purple_0 }
 hi["MsgArea"] = na
 hi["NonText"] = { fg = blue_0 }
 hi["Normal"] = { fg = white_0 }
-hi["NormalNC"] = { fg = black_5 }
-hi["Pmenu"] = { bg = blue_0 }
-hi["PmenuSel"] = { bg = blue_3 }
-hi["Question"] = { fg = green_1 }
-hi["Search"] = { fg = white_1, bg = green_1 }
+hi["NormalNC"] = { fg = black_6 }
+hi["Pmenu"] = { bg = black_3 }
+hi["PmenuSel"] = { bg = blue_2 }
+hi["Question"] = { fg = green_2 }
 hi["SignColumn"] = na
-hi["SpecialComment"] = { fg = yellow_1, gui = "bold,italic" } -- FIXME: and XXX: , etc.
-hi["SpecialKey"] = { fg = cyan_0, gui = bold }
+hi["SpecialComment"] = { fg = yellow_0, bold = true, italic = true } -- hi for TODO
+hi["SpecialKey"] = { fg = cyan_0, bold = true }
 hi["StatusLine"] = { bg = black_3 }
-hi["StatusLineNC"] = { fg = black_2, bg = black_2 }
-hi["Substitute"] = { fg = white_1, bg = green_1 }
-hi["Title"] = na
+hi["StatusLineNC"] = { fg = black_2 }
+hi["Title"] = { fg = blue_3, italic = true }
 hi["VertSplit"] = na
-hi["Visual"] = { fg = black_0, bg = blue_5 }
+hi["Visual"] = { fg = black_0, bg = blue_4 }
 hi["WarningMsg"] = na
 hi["Whitespace"] = { fg = blue_0 }
 hi["Yank"] = { bg = blue_1 }
@@ -85,30 +80,31 @@ hi["Yank"] = { bg = blue_1 }
 -- syntax
 -- default
 hi["__termbg"] = { bg = black_2 } -- invisible highlight for internal usage
-hi["Character"] = { fg = green_1 }
-hi["Comment"] = { fg = black_4, gui = ital }
-hi["Constant"] = { fg = purple_1 }
+hi["__termdarken"] = { bg = black_1 }
+hi["Character"] = { fg = green_2 }
+hi["Comment"] = { fg = black_5, italic = true }
+hi["Constant"] = { fg = purple_0 }
 hi["Error"] = { fg = red_2 }
-hi["Exception"] = { fg = green_1 }
-hi["Function"] = { fg = white_0, gui = "italic" }
+hi["Exception"] = { fg = green_2 }
+hi["Function"] = { fg = white_0, italic = true }
 hi["Identifier"] = na
-hi["Keyword"] = { fg = blue_4 }
+hi["Keyword"] = { fg = blue_3 }
 hi["Label"] = na
 hi["Number"] = { fg = red_0 }
 hi["Operator"] = { fg = yellow_0 }
 hi["OverLength"] = na
-hi["PreProc"] = na
+hi["PreProc"] = { fg = purple_0, bold = true }
 hi["Repeat"] = { fg = yellow_0 }
 hi["Special"] = na
 hi["Statement"] = { fg = yellow_0 }
 hi["Tag"] = na
-hi["Type"] = { fg = blue_5 }
-hi["Underlined"] = { gui = ul }
+hi["Type"] = { fg = blue_4 }
+hi["Underlined"] = { underline = true }
 
 -- Treesitter
-hi["TSNamespace"] = { fg = purple_1 }
-hi["TSStringEscape"] = { fg = green_0 }
-hi["TSStringRegex"] = { fg = green_0 }
+hi["TSNamespace"] = { fg = purple_0 }
+hi["TSStringEscape"] = { fg = green_1 }
+hi["TSStringRegex"] = { fg = green_1 }
 -- TSAnnotation = na,
 -- TSAttribute = na,
 -- TSConstBuiltin = na,
@@ -125,7 +121,6 @@ hi["TSStringRegex"] = { fg = green_0 }
 -- TSMath = na,
 -- TSMethod = na,
 -- TSNone = na,
--- TSNote = na,
 -- TSParameter = na,
 -- TSParameterReference = na,
 -- TSProperty = na,
@@ -139,10 +134,9 @@ hi["TSStringRegex"] = { fg = green_0 }
 
 -- Languages
 -- Markdown
-hi["markdownblockquote"] = { fg = green_1 }
-hi["markdownbold"] = { gui = bold }
-hi["markdownbolditalic"] = { gui = ul }
-hi["markdowncode"] = { fg = white_0, bg = black_1 }
+hi["markdownblockquote"] = { fg = green_2 }
+hi["markdownbold"] = { bold = true }
+hi["markdownbolditalic"] = { bold = true, italic = true }
 hi["markdowncodedelimiter"] = { fg = yellow_0 }
 hi["markdownfootnote"] = { fg = red_2 }
 hi["markdownfootnotedefinition"] = na
@@ -152,18 +146,18 @@ hi["markdownh3"] = { fg = yellow_0 }
 hi["markdownh4"] = { fg = yellow_0 }
 hi["markdownh5"] = { fg = yellow_0 }
 hi["markdownh6"] = { fg = yellow_0 }
-hi["markdownitalic"] = { gui = ital }
-hi["markdownlinebreak"] = { fg = green_0, gui = ital }
-hi["markdownlinktext"] = { fg = green_0, gui = ital }
+hi["markdownitalic"] = { italic = true }
+hi["markdownlinebreak"] = { fg = green_1, italic = true }
+hi["markdownlinktext"] = { fg = green_1, italic = true }
 hi["markdownlistmarker"] = { fg = yellow_0 }
 hi["markdownrule"] = na
+-- hi["markdowncode"] = { fg = white_0, bg = black_1 }
 
 -- plugins
 -- symbols outline
-hi["FocusedSymbol"] = { gui = ul }
 
 -- vim-highlighturl
-vim.g.highlighturl_guifg = blue_5
+vim.g.highlighturl_guifg = blue_4
 
 --[[
     Commonly linked groups:
@@ -197,38 +191,32 @@ vim.g.highlighturl_guifg = blue_5
         Typedef           -> Type           ✅
 ]]
 
-links["String"] = "Character"
-links["Boolean"] = "Constant"
-links["Float"] = "Number"
-links["Define"] = "PreProc"
-links["Include"] = "PreProc"
-links["Macro"] = "PreProc"
-links["PreCondit"] = "PreProc"
-links["Delimiter"] = "Special"
-links["SpecialChar"] = "Special"
-links["Todo"] = "SpecialComment"
-links["Conditional"] = "Statement"
-links["Exception"] = "Statement"
-links["StorageClass"] = "Type"
-links["Structure"] = "Type"
-links["Typedef"] = "Type"
+links["String"] = { link = "Character" }
+links["Boolean"] = { link = "Constant" }
+links["Search"] = { link = "CursorLine" }
+links["Substitute"] = { link = "CursorLine" }
+links["Float"] = { link = "Number" }
+links["Define"] = { link = "PreProc" }
+links["Include"] = { link = "PreProc" }
+links["Macro"] = { link = "PreProc" }
+links["PreCondit"] = { link = "PreProc" }
+links["Delimiter"] = { link = "Special" }
+links["SpecialChar"] = { link = "Special" }
+links["Todo"] = { link = "SpecialComment" }
+links["TSNote"] = { link = "SpecialComment" }
+links["Conditional"] = { link = "Statement" }
+links["Exception"] = { link = "Statement" }
+links["gitcommitSummary"] = { link = "Title" }
+links["StorageClass"] = { link = "Type" }
+links["Structure"] = { link = "Type" }
+links["Typedef"] = { link = "Type" }
 
-local function create_highlights(hi_tbl)
-    for group, hl in pairs(hi_tbl) do
-        local fg = hl.fg or "none"
-        local bg = hl.bg or "none"
-        local gui = hl.gui or "none"
-
-        vim.cmd(string.format("hi %s gui=%s guibg=%s guifg=%s", group, gui, bg, fg))
-    end
-end
-
-local function create_links(link_tbl)
-    for link_from, link_to in pairs(link_tbl) do
-        vim.cmd(string.format("hi! link %s %s", link_from, link_to))
+local function set_hi(hi_tbl)
+    for group, hi_opts in pairs(hi_tbl) do
+        vim.api.nvim_set_hl(0, group, hi_opts)
     end
 end
 
 -- call highlight
-create_highlights(hi)
-create_links(links)
+set_hi(hi)
+set_hi(links)
