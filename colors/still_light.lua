c1bb46a9bec463eacbac39b360c1e5ddfe648886
McAuley Penney <jacobmpenney@gmail.com>
2022-07-04 20:56:20 -0700

    feat: update colors, add new red

--- colors/still_light.lua
+++ colors/still_light.lua
@@ -2,103 +2,103 @@
     Author: MP
     Notes:
         hi groups: https://neovim.io/doc/user/syntax.html#highlight-groups
-        bg, set in term cfg: #212121
+        bg, set in term cfg: #1d1d1d
+
+    TODO:
+      1. adjust type
+      3. find a color for constants and keywords
+        • too similar
 ]]
 
-local na = {}
-local black = {
-    [0] = "#000000",
-    [1] = "#141414",
-    [2] = "#212121",
-    [3] = "#2d2d2d",
-    [4] = "#424242",
-    [5] = "#4f4f4f",
-    [6] = "#666666",
-    [7] = "#c1c1c1",
-}
 
-local white = {
-    [0] = "#DBD3C9",
+local na = {}
+local gray = {
+  [0] = "#000000",
+  [1] = "#1d1d1d",
+  [2] = "#292929",
+  [3] = "#3e3e3e",
+  [4] = "#4f4f4f",
+  [5] = "#606060",
+  [6] = "#DBD3C9",
 }
 
 local green = {
-    [0] = "#4b8b51",
-    [1] = "#5C8145",
-    [2] = "#8CA576",
-    [3] = "#B3C3A4",
+  [0] = "#4b8b51",
+  [1] = "#5C8145",
+  [2] = "#8CA576",
+  [3] = "#B3C3A4",
 }
 
 local blue = {
-    [0] = "#434649",
-    [1] = "#616E88",
-    [2] = "#8296b0",
-    [3] = "#8D98A7",
+  [1] = "#616E88",
+  [2] = "#8296b0",
+  [3] = "#9aabbc",
 }
 
 local red = {
-    [0] = "#e26d5c",
-    [1] = "#FF0000",
+  [0] = "#B48EAD",
+  [1] = "#e26d5c",
+  [2] = "#FF0000",
 }
 
 local yellow = {
-    [0] = "#FEAD4C",
-    [1] = "#FFC15E",
+  [0] = "#FEAD4C"
 }
 
-local purple_0 = "#90708C"
-local cyan_0 = "#00CECB"
 
 -- highlight dictionaries
 local hl = {}
 local link = {}
 
 local function set_hl(hl_tbl)
-    for group, opts in pairs(hl_tbl) do
-        vim.api.nvim_set_hl(0, group, opts)
-    end
+  for group, opts in pairs(hl_tbl) do
+    vim.api.nvim_set_hl(0, group, opts)
+  end
 end
 
 --------------------------------------------------
 -- Hightlights
 --------------------------------------------------
+
+-- user-defined
+hl["__termbg"] = { bg = gray[1] }
+hl["__success"] = { fg = green[0] }
+
 -- UI
+hl["Cursor"] = { bg = red[2] }
 hl["ColorColumn"] = na
 hl["Conceal"] = na
-hl["Cursor"] = { bg = yellow[1] }
-hl["CursorLine"] = { fg = yellow[1], bg = black[4], bold = true }
+hl["CursorLine"] = { fg = yellow[0], bg = gray[3], bold = true }
 hl["CursorLineNr"] = na
-hl["Directory"] = { fg = blue[2] }
-hl["Folded"] = { fg = blue[0], italic = true }
-hl["MatchParen"] = { fg = red[1], bold = true }
-hl["ModeMsg"] = { fg = green[1] }
-hl["MoreMsg"] = { fg = purple_0 }
+hl["Directory"] = { fg = blue[2], italic = true }
+hl["Error"] = { fg = red[2] }
+hl["MatchParen"] = { fg = red[2], bold = true }
+hl["ModeMsg"] = { fg = green[0] }
 hl["MsgArea"] = na
-hl["NonText"] = { fg = blue[0] }
-hl["Normal"] = { fg = white[0], bg = black[2] }
-hl["NormalNC"] = { fg = black[7] }
-hl["Pmenu"] = { bg = black[3] }
+hl["NonText"] = { fg = gray[4], italic = true }
+hl["Normal"] = { fg = gray[6], bg = gray[1] }
 hl["PmenuSel"] = { bg = blue[1] }
-hl["Question"] = { fg = green[3] }
-hl["SpecialComment"] = { fg = black[0], bg = yellow[0] }
-hl["SpecialKey"] = { fg = cyan_0, bold = true }
-hl["StatusLine"] = { bg = black[3] }
-hl["Title"] = { fg = blue[2], italic = true }
-hl["VertSplit"] = { fg = black[3], bold = true }
-hl["Visual"] = { fg = white[0], bg = black[5] }
-hl["WarningMsg"] = na
-hl["Yank"] = { fg = black[0], bg = yellow[0] }
+hl["Question"] = { fg = green[0] }
+hl["Search"] = na
+hl["SpecialComment"] = { fg = gray[0], bg = yellow[0] }
+hl["SpecialKey"] = { fg = yellow[0], bold = true }
+hl["StatusLine"] = { bg = gray[2] }
+hl["VertSplit"] = { fg = gray[2] }
+hl["Visual"] = { bg = gray[3] }
+hl["Yank"] = { fg = gray[0], bg = yellow[0] }
+
+
 
 -- Syntax
 hl["Character"] = { fg = green[2] }
-hl["Comment"] = { fg = black[6], italic = true }
-hl["Constant"] = { fg = purple_0 }
-hl["Exception"] = { fg = green[3] }
-hl["Function"] = { fg = white[0], italic = true }
+hl["Comment"] = { fg = gray[5], italic = true }
+hl["Constant"] = { fg = red[0] }
+hl["Exception"] = { fg = red[2] }
+hl["Function"] = { italic = true }
 hl["Identifier"] = na
 hl["Keyword"] = { fg = blue[2] }
-hl["Label"] = na
-hl["Number"] = { fg = red[0] }
-hl["PreProc"] = { bg = black[0] }
+hl["Number"] = { fg = red[1] }
+hl["PreProc"] = { bg = gray[0] }
 hl["Special"] = na
 hl["Statement"] = { fg = yellow[0] }
 hl["String"] = { fg = green[3] }
@@ -108,20 +108,19 @@ hl["Type"] = { fg = blue[3] }
 -- LSP --
 -- Diagnostics
 for type, color in pairs({
-    Error = "Red",
-    Warn = "Orange",
-    Hint = "LightGray",
-    Info = "LightBlue",
+  Error = "Red",
+  Warn = "Orange",
+  Hint = "LightGray",
+  Info = "LightBlue",
 }) do
-    hl["DiagnosticUnderline" .. type] = { sp = color, underline = true }
+  hl["DiagnosticUnderline" .. type] = { sp = color, underline = true }
 end
 
--- TODO: adjust
 -- Handlers
-hl["LspSignatureActiveParameter"] = { bg = red[0] }
+hl["LspSignatureActiveParameter"] = { fg = red[1], italic = true }
 
 -- Treesitter
-hl["TSNamespace"] = { fg = purple_0 }
+hl["TSNamespace"] = { fg = blue[1] }
 hl["TSStringEscape"] = { fg = green[1] }
 hl["TSStringRegex"] = { fg = green[1] }
 --[[
@@ -154,24 +153,20 @@ hl["TSStringRegex"] = { fg = green[1] }
 
 -- Language-specific --
 -- Markdown
-hl["markdownHeadingDelimiter"] = { fg = yellow[0] }
-hl["markdownCodeBlock"] = { fg = yellow[0] }
-hl["markdownbold"] = { bold = true }
-hl["markdownbolditalic"] = { bold = true, italic = true }
-hl["markdownCodeDelimiter"] = { fg = yellow[0] }
-hl["markdownfootnote"] = { fg = red[1] }
-hl["markdownfootnotedefinition"] = na
-hl["markdownitalic"] = { italic = true }
-hl["markdownlinebreak"] = { fg = green[1], italic = true }
-hl["markdownlinktext"] = { fg = green[1], italic = true }
-hl["markdownlistmarker"] = { fg = yellow[0] }
-hl["markdownrule"] = na
--- hl["markdownCode"] = { bg = black[1] }
+-- hl["markdownHeadingDelimiter"] = { fg = yellow[0] }
+-- hl["markdownCodeBlock"] = { fg = yellow[0] }
+-- hl["markdownbold"] = { bold = true }
+-- hl["markdownbolditalic"] = { bold = true, italic = true }
+-- hl["markdownCodeDelimiter"] = { fg = yellow[0] }
+-- hl["markdownfootnote"] = { fg = red[1] }
+-- hl["markdownfootnotedefinition"] = na
+-- hl["markdownitalic"] = { italic = true }
+-- hl["markdownlinebreak"] = { fg = green[1], italic = true }
+-- hl["markdownlinktext"] = { fg = green[1], italic = true }
+-- hl["markdownlistmarker"] = { fg = yellow[0] }
+-- hl["markdownrule"] = na
 
--- user-defined
-hl["__termbg"] = { bg = black[2] }
-hl["__termdarken"] = { bg = black[1] }
-hl["__success"] = { fg = green[0] }
+
 
 -- Plugins --
 -- itchyny/vim-highlighturl
@@ -193,7 +188,7 @@ vim.g.highlighturl_guifg = blue[3]
         Function          -> Identifier
         Include           -> PreProc        
         Keyword           -> Statement
-        Label             -> Statement
+        Label             -> Statement      
         Macro             -> PreProc        
         Number            -> Constant
         Operator          -> Statement
@@ -215,18 +210,25 @@ vim.g.highlighturl_guifg = blue[3]
 -- UI --
 link["Delimiter"] = { link = "Special" }
 link["EndOfBuffer"] = { link = "NonText" }
-link["Error"] = { link = "DiagnosticError" }
-link["ErrorMsg"] = { link = "DiagnosticError" }
+link["ErrorMsg"] = { link = "Error" }
+link["FloatBorder"] = { link = "StatusLine" }
+link["Folded"] = { link = "NonText" }
 link["FoldColumn"] = { link = "ColorColumn" }
+link["Label"] = { link = "Keyword" }
 link["LineNr"] = { link = "NonText" }
+link["MoreMsg"] = { link = "ModeMsg" }
+link["Pmenu"] = { link = "StatusLine" }
+link["QuickFixLine"] = { link = "CursorLine" }
 link["Search"] = { link = "CursorLine" }
-link["SignColumn"] = { link = "ColorColumn" }
+link["SignColumn"] = { link = "StatusLineNC" }
 link["SpecialChar"] = { link = "Special" }
 link["StatusLineNC"] = { link = "__termbg" }
 link["Substitute"] = { link = "CursorLine" }
-link["TSNote"] = { link = "SpecialComment" }
 link["TabLineFill"] = { link = "__termbg" }
+link["Title"] = { link = "Directory" }
 link["Todo"] = { link = "SpecialComment" }
+link["TSNote"] = { link = "SpecialComment" }
+link["WarningMsg"] = { link = "Error" }
 link["Whitespace"] = { link = "NonText" }
 
 -- Syntax --
@@ -243,11 +245,13 @@ link["Repeat"] = { link = "Statement" }
 link["StorageClass"] = { link = "Type" }
 link["Structure"] = { link = "Type" }
 link["Typedef"] = { link = "Type" }
-link["gitcommitSummary"] = { link = "Title" }
+link["gitcommitSummary"] = { link = "Directory" }
 
 -- Plugins --
 -- simrat39/symbols-outline.nvim
 link["FocusedSymbol"] = { link = "CursorLine" }
+-- lukas-reineke/virt-column.nvim
+link["VirtColumn"] = { link = "VertSplit" }
 
 --------------------------------------------------
 -- Execute
